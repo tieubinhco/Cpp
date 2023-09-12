d696b9a4e807e9c1983e4c3e3befe84d35eb04ba
Tieu Binh <tieubinhco@gmail.com>
2023-09-13 00:19:29 +0700

    Update post_pre_increment.c

--- C/post_pre_increment.c
+++ C/post_pre_increment.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
   
+
+void increment (int *ptr)
+{ (*ptr)++;
+}
+
+
 int main()
 {
     int x = 5, y = 10;
@@ -7,5 +13,10 @@ int main()
     
     printf("x= %d , y= %d , z= %d ", x,y,z);
     // "x= 6 , y= 10 , z= 15 "
+
+    int t=5;
+    int *ptr=&t;
+    increment(ptr);
+    printf("\nValue of x: %d", t);
     return 0;
 }
